a7077e86ae27f73ef4ff41e490e7be0a4da3eeb4
Alan Macgowan <alanmacgowan@gmail.com>
2017-09-28 20:54:44 -0300

    refactor($browser): added code
    added initial code

---
+++ demo/demo.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-app',
+  template: '<app-hello-world></app-hello-world>'
+})
+export class DemoComponent {}
---
+++ src/grid/grid-row.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { IEntity } from '../IEntity';
+import { IGridRow } from './IGridRow';
+
+@Component({
+  selector: 'grid-row',
+  templateUrl: 'grid-row.component.html'
+})
+export class GridRowComponent implements OnInit {
+  @Input() items: IGridRow[] = [];
+  @Output() onItemEdit: EventEmitter<IEntity> = new EventEmitter();
+  @Output() onItemDelete: EventEmitter<IEntity> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  editItem(entity: IEntity, event?: MouseEvent) {
+    if (event) {
+      event.preventDefault();
+    }
+    this.onItemEdit.emit(entity);
+  }
+
+  deleteItem(entity: IEntity, event?: MouseEvent) {
+    if (event) {
+      event.preventDefault();
+    }
+    this.onItemDelete.emit(entity);
+  }
+}
